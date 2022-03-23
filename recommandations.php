119beedc2b6f763f6fdf582ea42a52959599cacd
VanTej <vanessatejedor@gmail.com>
2022-03-23 12:24:46 +0100

    correction et php recommandation

---
+++ recommandations.php
@@ -0,0 +1,22 @@
+<?php
+
+$recommandations = [
+    ['img-Perso' => 'url',
+    'name' => 'nom du perso',
+    'recommandation' => 'ce qu\'il a dit',
+],
+    ['img-Perso' => 'url',
+    'name' => 'nom du perso',
+    'recommandation' => 'ce qu\'il a dit',
+],
+    ['img-Perso' => 'url',
+    'name' => 'nom du perso',
+    'recommandation' => 'ce qu\'il a dit',
+],
+    ['img-Perso' => 'url',
+    'name' => 'nom du perso',
+    'recommandation' => 'ce qu\'il a dit',
+],
+];
+
+var_dump($recommandations);
